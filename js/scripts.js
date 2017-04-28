8a278d4de7c05c633e40f73c4cff4e0e15e91e09
ivanfrontend <ivanfrondend111950@mail.ru>
2017-04-28 04:01:50 +0400

    mygit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$('.saitbarmenu').click(function(){
+	$('.saitbarmenu').removeClass('activPunkt');
+	$(this).addClass('activPunkt');
+});
+
+$('.saitbarmenu2').click(function(){
+	$('.saitbarmenu2').removeClass('activPunkt2');
+	$(this).toggleClass('activPunkt2');	
+});
